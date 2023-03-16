fa7a6a9ece284e40ad55b32f96769a845e4958d3
Eve-Kamau <evelyne.kamau@student.moringaschool.com>
2023-03-17 00:26:35 +0300

    commit tree bc43db05cfea879594e7c608c1c421ae826e2a4f

--- looping.rb
+++ looping.rb
@@ -1,24 +1,58 @@
+#Using a while or until loop outputs numbers starting at 10 count down to 1. 
+#After reaching 1, print out "Happy New Year!"
 def happy_new_year
-  # your code here
+    i = 10
+    while i >= 1
+      puts "#{i}"
+      i -= 1
+    end
+      puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
-    "FizzBuzz"
+    puts "FizzBuzz"
   elsif num % 3 == 0
-    "Fizz"
+    puts "Fizz"
   elsif num % 5 == 0
-    "Buzz"
+   puts  "Buzz"
   else
-    num
+   puts num
   end
 end
 
 def fizzbuzz_printer
-  # your code here
-end
+  (1..100).each {|n| fizzbuzz n}
 
-def reverse_string(str)
-  # your code here
+  # (num1.upto num2).each do |n|
+  #   n = ""
+  #     if n % 3 == 0 && n % 5 == 0
+  #       puts "FizzBuzz"
+  #     elsif n % 3 == 0
+  #     puts "Fizz"
+  #     elsif n % 5 == 0
+  #       puts "Buzz"
+  #     else
+  #     puts n
+  #     end
+  #   end  
 end
+# fizzbuzz_printer(1,100)
+
+#Takes one argument, a string, & reverses it. Don't use .reverse method. 
+#Instead, loop through the characters in the input string and reverse it.
+def reverse_string(str)
+  altered_str = ''
+ 
+  i = 0 
+  while i < str.length 
+   altered_str = str[i] + altered_str
+   i += 1 
+  end
+  
+  return altered_str
+ end
+
+reverse_string("hello")
+# => "olleh"
